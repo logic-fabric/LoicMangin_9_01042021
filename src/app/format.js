a8e85d670589deff64ad2a6fa7d50f6557b37143
logic-fabric <lowic@logic-fabric.fr>
2021-05-05 15:59:49 +0200

    refactor: app/format.js

--- src/app/format.js
+++ src/app/format.js
@@ -1,10 +1,15 @@
 export const formatDate = (dateStr) => {
   const date = new Date(dateStr);
-  const ye = new Intl.DateTimeFormat("fr", { year: "numeric" }).format(date);
-  const mo = new Intl.DateTimeFormat("fr", { month: "short" }).format(date);
-  const da = new Intl.DateTimeFormat("fr", { day: "2-digit" }).format(date);
-  const month = mo.charAt(0).toUpperCase() + mo.slice(1);
-  return `${parseInt(da)} ${month.substr(0, 3)}. ${ye.toString().substr(2, 4)}`;
+
+  const year = new Intl.DateTimeFormat("fr", { year: "numeric" }).format(date);
+  const month = new Intl.DateTimeFormat("fr", { month: "short" }).format(date);
+  const day = new Intl.DateTimeFormat("fr", { day: "2-digit" }).format(date);
+
+  month = month.charAt(0).toUpperCase() + month.slice(1);
+
+  return `${parseInt(day)} ${month.substr(0, 3)}. ${year
+    .toString()
+    .substr(2, 4)}`;
 };
 
 export const formatStatus = (status) => {
