6bba61b8e9ec9ec49582bb6e6155213420f5c221
logic-fabric <lowic@logic-fabric.fr>
2021-05-06 12:21:17 +0200

    refactor: views/LoginUI.js

--- src/views/LoginUI.js
+++ src/views/LoginUI.js
@@ -1,7 +1,5 @@
-
-export default () => {
-
-  return(`
+export default function LoginUI() {
+  return `
       <div class="page-div">
         <div class="row">
           <div class="title-container"> 
@@ -43,6 +41,5 @@ export default () => {
           </div>
         </div>
       </div>
-  `)
+  `;
 }
-  
