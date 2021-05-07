8ff20c9dbfbed47d12fa24bcb66bc1234c127037
logic-fabric <lowic@logic-fabric.fr>
2021-05-07 17:30:15 +0200

    refactor: __tests__/VerticalLayout.js

--- src/__tests__/VerticalLayout.js
+++ src/__tests__/VerticalLayout.js
@@ -1,19 +1,22 @@
-import { screen } from "@testing-library/dom"
-import VerticalLayout from "../views/VerticalLayout"
-import { localStorageMock } from "../__mocks__/localStorage.js"
+import VerticalLayout from "../views/VerticalLayout";
 
+import { localStorageMock } from "../__mocks__/localStorage.js";
 
-describe('Given I am connected as Employee', () => {
-  test("Then Icons should be rendered", () => {
-    Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+import { screen } from "@testing-library/dom";
+
+describe("GIVEN I am connected as Employee", () => {
+  test("THEN icons should be rendered", () => {
+    Object.defineProperty(window, "localStorage", { value: localStorageMock });
     const user = JSON.stringify({
-      type: 'Employee'
-    })
-    window.localStorage.setItem('user', user)
-    const html = VerticalLayout(120)
-    document.body.innerHTML = html
-    expect(screen.getByTestId('icon-window')).toBeTruthy()
-    expect(screen.getByTestId('icon-mail')).toBeTruthy()
-  })
+      type: "Employee",
+    });
+    window.localStorage.setItem("user", user);
 
-})
+    const html = VerticalLayout(120);
+    document.body.innerHTML = html;
+
+    expect(screen.getByTestId("icon-window")).toBeTruthy();
+
+    expect(screen.getByTestId("icon-mail")).toBeTruthy();
+  });
+});
