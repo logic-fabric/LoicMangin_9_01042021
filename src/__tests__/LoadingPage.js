958fc4389163692703cafbf2daf149fc90965943
logic-fabric <lowic@logic-fabric.fr>
2021-05-07 17:03:16 +0200

    refactor: __tests__/LoadingPage.js

--- src/__tests__/LoadingPage.js
+++ src/__tests__/LoadingPage.js
@@ -1,12 +1,14 @@
-import { screen } from "@testing-library/dom"
-import LoadingPage from "../views/LoadingPage.js"
+import LoadingPage from "../views/LoadingPage.js";
 
-describe('Given I am connected on app (as an Employee or an HR admin)', () => {
-  describe('When LoadingPage is called', () => {
-    test(('Then, it should render Loading...'), () => {
-      const html = LoadingPage()
-      document.body.innerHTML = html
-      expect(screen.getAllByText('Loading...')).toBeTruthy()
-    })
-  })
-})
+import { screen } from "@testing-library/dom";
+
+describe("GIVEN I am connected on app (as an Employee or an HR admin)", () => {
+  describe("WHEN LoadingPage is called", () => {
+    test("THEN it should render Loading...", () => {
+      const html = LoadingPage();
+      document.body.innerHTML = html;
+      
+      expect(screen.getAllByText("Loading...")).toBeTruthy();
+    });
+  });
+});
