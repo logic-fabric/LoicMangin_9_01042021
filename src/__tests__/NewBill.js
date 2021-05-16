213012b610eebc8df4e45ac84babcc0e9ec2d850
logic-fabric <lowic@logic-fabric.fr>
2021-05-16 09:39:30 +0200

    test: add a test about containers/NewBill.js to check redirection after submitting the form

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -1,15 +1,50 @@
 import NewBill from "../containers/NewBill.js";
 import NewBillUI from "../views/NewBillUI.js";
+import { ROUTES } from "../constants/routes.js";
 
-//import { screen } from "@testing-library/dom";
+import { localStorageMock } from "../__mocks__/localStorage.js";
 
-describe("GIVEN I am connected as an employee", () => {
-  describe("WHEN I am on NewBill Page", () => {
-    test("THEN ...", () => {
-      const html = NewBillUI();
-      document.body.innerHTML = html;
+import { fireEvent, screen } from "@testing-library/dom";
 
-      // TO DO: write tests
+describe("GIVEN I am connected as an Employee", () => {
+  describe("WHEN I am on NewBill page and I submit a correct form", () => {
+    test("THEN I should be redirected to Bills page", () => {
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname });
+      };
+
+      Object.defineProperty(window, "localStorage", {
+        value: localStorageMock,
+      });
+
+      const firestore = null;
+
+      window.localStorage.setItem(
+        "user",
+        JSON.stringify({
+          type: "Employee",
+        })
+      );
+
+      document.body.innerHTML = NewBillUI();
+
+      const newBillContainer = new NewBill({
+        document,
+        onNavigate,
+        firestore,
+        localStorage: window.localStorage,
+      });
+
+      const handleSubmit = jest.fn(newBillContainer.handleSubmit);
+      newBillContainer.fileName = "image.jpg";
+
+      const newBillForm = screen.getByTestId("form-new-bill");
+      newBillForm.addEventListener("submit", handleSubmit);
+      fireEvent.submit(newBillForm);
+
+      expect(handleSubmit).toHaveBeenCalled();
+
+      expect(screen.getAllByText("Mes notes de frais")).toBeTruthy();
     });
   });
 });
